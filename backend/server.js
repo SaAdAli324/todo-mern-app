8e63d5907322aad4245d69c030a1b2da2f602e6a
SaAdAli324 <rajr29880@gmail.com>
2025-08-29 21:36:18 +0500

    Fix backend static path to dist folder for frontend

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
 // ✅ Express 5+ catch-all for SPA routes
 app.get('/{*path}', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
 })
 
 // ---------- START SERVER ----------
